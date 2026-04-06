8158f6188fdc24bae2b4971526edec4c5e38a792
Eldor Karimov <hacktivist19021@gmail.com>
2026-04-06 10:42:32 +0500

    lang changed

--- frontend/src/i18n/index.ts
+++ frontend/src/i18n/index.ts
@@ -1,13 +1,11 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
-import LanguageDetector from 'i18next-browser-languagedetector'
 
 import uz from './locales/uz.json'
 import ru from './locales/ru.json'
 import en from './locales/en.json'
 
 i18n
-  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources: {
@@ -15,14 +13,10 @@ i18n
       ru: { translation: ru },
       en: { translation: en },
     },
+    lng: 'uz',
     fallbackLng: 'uz',
-    supportedLngs: ['uz', 'ru', 'en'],
     interpolation: {
       escapeValue: false,
-    },
-    detection: {
-      order: ['localStorage', 'navigator'],
-      caches: ['localStorage'],
     },
   })
 
